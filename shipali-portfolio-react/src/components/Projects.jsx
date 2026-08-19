ccbcd2ec2ba5f8d76b68c1f8f5925d2b439d760b
shipalibhandary <shipalibhandary@gmail.com>
2026-08-19 20:17:57 +0530

    Add PassBill project and enhance descriptions; adjust animation delay

--- shipali-portfolio-react/src/components/Projects.jsx
+++ shipali-portfolio-react/src/components/Projects.jsx
@@ -5,15 +5,25 @@ function Projects() {
     {
       title:'Case Velora- Airbnb Clone',
       image:'/images/project-screenshots/Airbnb.png',
-      description:'A clone of Airbnb website built using React + vite.',
+      description:'A clone of Airbnb website built using React + vite. Includes static property data, multiple property images, scroll locking, responsive design, and smooth user interactions.',
       github:'https://github.com/shipalibhandary/Airbnb/tree/main/airbnb',
       live :'https://airbnb-seven-bice.vercel.app/',
       colorClass:'dark'
     },
+
+    {
+      title:"PassBill - Pastry Billing",
+      image:'/images/project-screenshots/Passbill.png',
+      description:"A billing application for a pastry shop built using React + vite. Features include product management, bill generation, and a user-friendly interface for efficient billing operations.",
+      github:'https://github.com/shipalibhandary/PasBil',
+      colorClass:'beige'
+
+    },
+
     {
       title: 'Telegram-Dashboard',
       image: '/images/project-screenshots/telegram.png',
-      description: 'An interactive dashboard for analyzing Telegram channel data.',
+      description: 'An interactive dashboard for analyzing Telegram channel data built using Python + Streamlit. . Features data visualization, chat analysis, and an interactive dashboard for exploring communication patterns and personality-related results.',
       github: 'https://github.com/shipalibhandary/telegram_channel_dashboard',
       live: 'https://telegramchanneldashboard.streamlit.app/',
       colorClass: 'beige'
@@ -31,7 +41,7 @@ function Projects() {
     {
       title: 'ElectraNexa',
       image: 'images/project-screenshots/Electranexa.png',
-      description: 'An e-commerce website for a seamless shopping experience.',
+      description: 'An e-commerce website for a seamless shopping experience. Implemented filtering and responsive layouts using JavaScript and Bootstrap. ',
       github: 'https://github.com/shipalibhandary/ElectraNexa',
       live: '',
       colorClass: 'dark'
